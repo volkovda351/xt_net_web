90df40b33e177c94d287d2bd70c9075770e336a5
Daniil Volkov <volkovda351@gmail.com>
2020-08-02 13:40:12 +0400

    add task 4
    program uses git, so git should be preinstalled

---
+++ Task 4/Task 4.1 Files/Task 4.4.1 File management system/Task 4.4.1 File management system/Git.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4._4._1_File_management_system
+{
+	public static class Git
+	{
+		public static void Init()
+		{
+			CommandLine.Execute("git init");
+		}
+
+		public static void Add()
+		{
+			CommandLine.Execute("git add -A *.txt");
+		}
+
+		public static void Commit(string time)
+		{
+			CommandLine.Execute($"git commit -m {time}");
+		}
+
+		public static void ResetHard(Commit commit)
+		{
+			CommandLine.Execute($"git reset --hard {commit.Hash}");
+		}
+
+		public static string LogOneline()
+		{
+			return CommandLine.Execute("git log --oneline");
+		}
+	}
+}
